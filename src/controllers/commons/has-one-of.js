0b7aed54d11e7ecfac68dde5b42e578c16466191
peusoaresf <psoaresferraz@gmail.com>
2022-01-28 12:49:25 -0300

    feat: add role checking mechanism

--- src/controllers/commons/has-one-of.js
+++ src/controllers/commons/has-one-of.js
@@ -1,9 +1,33 @@
+const agentsRepository = require('../../database/repositories/agents-repository')
 const logger = require('../../utils/logger')
 
-const hasOneOf = (...role) => (req, res, next) => {
+const hasOneOf = (...roles) => async (req, res, next) => {
   logger.info({
-    message: `Checking if user has one of roles ${role}`,
+    message: 'Checking if user has one of roles',
+    roles,
+    agentId: req.agentId,
   })
+
+  const agent = await agentsRepository.getAgentById({
+    id: req.agentId,
+  })
+
+  if (!agent) {
+    const error = new Error('Agent is not found')
+    error.status = 404
+    error.name = 'AgentNotFoundError'
+
+    return next(error)
+  }
+
+  if (!agent.roles.some(role => roles.includes(role))) {
+    const error = new Error('Agent is not allowed to access this resource')
+    error.status = 403
+    error.name = 'ForbiddenError'
+
+    return next(error)
+  }
+
   next()
 }
 
